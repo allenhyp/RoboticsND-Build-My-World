31097e64d691a60b87b58b1a98c763531c8a6933
bboypupu <bboypupu@gmail.com>
2021-03-16 00:44:35 -0700

    Ignore build and fix the display message

--- script/hello_world_plugin.cpp
+++ script/hello_world_plugin.cpp
@@ -3,7 +3,7 @@
 namespace gazebo {
     class WorldPluginHelloWorld : public WorldPlugin {
         public: WorldPluginHelloWorld() : WorldPlugin() {
-            printf("Hello World! (for project 1)\n");
+            printf("Welcome to Allen's World!\n");
         }
 
         public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf) {
